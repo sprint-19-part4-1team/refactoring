97f623a18255a0443fff084ec2baafe6c6cb1948
aahreum <cocoding420@gmail.com>
2025-12-09 22:46:15 +0900

    feat: 닉네임 뷰 페이지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import styles from "./page.module.css";
+import useUserProfile from "@/features/user/ui/useUserProfile";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <div></div>;
+  const { user, error } = useUserProfile();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (error) {
+      router.replace("/not-found");
+    }
+  }, [error, router]);
+
+  if (!user) {
+    return <div className={styles.container}>로딩중..</div>;
+  }
+
+  return <div className={styles.container}>닉네임: {user.nickname}</div>;
 }
