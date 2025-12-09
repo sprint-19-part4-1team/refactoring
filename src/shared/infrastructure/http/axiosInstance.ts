15d6aee2a95796cb8a97801518d82828b6d42a7c
aahreum <cocoding420@gmail.com>
2025-12-09 21:51:54 +0900

    Feat: getAccessToken 함수 연결

--- src/shared/infrastructure/http/axiosInstance.ts
+++ src/shared/infrastructure/http/axiosInstance.ts
@@ -1,3 +1,4 @@
+import { getAccessToken } from "@/features/user/infrastructure/tokenStorage";
 import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
@@ -7,10 +8,9 @@ const api = axios.create({
   timeout: 5000,
 });
 
-// 이것도 분리해야하나....????
 api.interceptors.request.use((config) => {
-  // TODO: 토큰 불러오는 로직 필요 (일단 로컬스토리지에서 관리)
-  const token = "토큰 로직 필요";
+  // 일단 간단하게 로컬스토리지에서 관리
+  const token = getAccessToken();
   if (token) {
     config.headers = config.headers ?? {};
     config.headers.Authorization = `Bearer ${token}`;
